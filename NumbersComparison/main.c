0571fdcc2af10261a07f04af6553c9f46656b152
Ali Hammoud <100542677+Alihm92754@users.noreply.github.com>
2024-05-28 21:42:05 +0300

    Add files via upload

---
+++ NumbersComparison/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+	float num1, num2;
+	printf("Enter the first number: ");
+	fflush(stdout);
+	scanf("%f",&num1);
+	printf("Enter the second number: ");
+	fflush(stdout);
+	scanf("%f",&num2);
+	int inum1, inum2;
+	inum1 = num1;
+	inum2 = num2;
+
+	if((inum1 != num1) || (inum2 != num2)) printf("Warning -comparing only integer part \n");
+
+	if(inum1 == inum2){
+		printf("Numbers are equal");
+	}
+	else {
+		if (inum1 < inum2){
+			printf("Num2 is Bigger");
+		}
+		else{
+			printf("Num1 is Bigger");
+		}
+	}
+
+	return 0;
+
+}
